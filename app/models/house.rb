8603b3c16d42c57b0060d2b8f8593817264d66a5
Simranjeet Singh Hunjan <simranjeetsinghhunjan@gmail.com>
2018-09-27 14:11:09 -0500

    Initial commit

---
+++ app/models/house.rb
@@ -0,0 +1,6 @@
+class House < ApplicationRecord
+  has_many :students
+
+  validates :name, presence: true, uniqueness: true
+  validates :points, numericality: {only_integer: true}
+end
---
+++ app/models/student.rb
@@ -0,0 +1,5 @@
+class Student < ApplicationRecord
+  belongs_to :house
+
+  validates :name, presence: true
+end
