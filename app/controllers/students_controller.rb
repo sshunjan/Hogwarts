d99d91c5c742aefe787515c9f80c6fe50e965429
Simranjeet Singh <sshunjan97@gmail.com>
2018-09-27 15:22:59 -0500

    Implemented the index action and view.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,12 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
+
+  # Automatically opens and renders the associated view.
+  # app/views/students.index/html.erb
+  # Also all instance variables created within the
+  # action are shared within the associated view
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'students/index'
-  get 'students/show'
+  # GET /students/index => index in students_controller.rb
+  get 'students' , to: 'students#index'
+  # URL path            contoller action
+
+  root to: 'students#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
