813e105c1e9c2c0000ca86f893f1b4315de00b6f
Simranjeet Singh <sshunjan97@gmail.com>
2018-09-28 13:35:57 -0500

    Added links and restful routes.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,10 +4,11 @@ class StudentsController < ApplicationController
   end
 
   # Automatically opens and renders the associated view.
-  # app/views/students.index/html.erb
+  # app/views/students.index/html.erb.
   # Also all instance variables created within the
   # action are shared within the associated view
 
   def show
+    @student = Student.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   # GET /students/index => index in students_controller.rb
-  get 'students' , to: 'students#index'
-  # URL path            contoller action
+  # get 'students' ,to: 'students#index'
+  # # URL path            contoller action
+  # get 'students/:id' ,to: 'students#show', id: /\d+/
+
+  # RESTFUL routes
+  resources :students, only: [:index, :show]
+  # index    GET /students
+  # show     GET /students/:id
+  # new      GET /student/new
+  # create   POST /students
+  # edit     GET /students/:id/edit
+  # update   PATCH /students/:id
+  # delete   DELETE /students/:id
 
   root to: 'students#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
